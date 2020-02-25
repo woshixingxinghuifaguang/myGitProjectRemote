d8701c8f3f4ce8434e1ca3734b7517e1ba8dd794
黄泓薇 <826764177@qq.com>
2020-02-25 19:01:41 +0800

    merge origin develop

--- gitflowdemo/hello.c
+++ gitflowdemo/hello.c
@@ -3,5 +3,6 @@
 int main(int arg,char* argv[]){
     printf("inited by SCM.\n");
     printf("develop branch created by SCM.\n");
-    printf("hello world!\n");
+    printf("function02 is added by dev02.\n");
+    printf("function01 added by dev01.\n);
 }
